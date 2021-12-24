68b9959da22b5b15606e7ec604953d4e7d839343
Danila Pisarev <jobdan@bk.ru>
2021-12-24 11:50:09 +0300

    Added the checkbox to add commision

--- src/components/commision-tab/CommisionTab.tsx
+++ src/components/commision-tab/CommisionTab.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import UserInput from "../common/user-input/UserInput";
+import './commision-tab.css';
 
 export const CommisionTab = () => {
+    const [checked, setChecked] = useState(false);
+
     return (
         <div className="commitsion-tab">
-            <UserInput 
-                labelName="Minting price for users (EVERs)"
-                inputName="Type price"
-            />
+            <div className="commision-tab__content">
+                <div className="commision-checkbox mb-3">
+                    <label>
+                        <input type="checkbox"
+                            onChange={() => setChecked(!checked)} />
+                        Commision to collection owner
+                    </label>
+                    <div className={"commsion-input " + (checked ? 'active' : '')}>
+                        <UserInput
+                            labelName="Commision (%)"
+                            inputName="Type commision here"
+                        />
+                    </div>
+                </div>
+                <UserInput
+                    labelName="Minting price for users (EVERs)"
+                    inputName="Type price"
+                />
+            </div>
         </div>
     );
 }
