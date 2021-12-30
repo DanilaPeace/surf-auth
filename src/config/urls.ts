59d26b9b4e7aa3bc629e82af233202866e0bdc9a
Danila Pisarev <jobdan@bk.ru>
2021-12-30 22:07:05 +0300

    Added paths for fetch requests

---
+++ src/config/urls.ts
@@ -0,0 +1,5 @@
+export const global_urls = {
+    COLLECTION_LIST_URL: 'http://localhost:4001/collection-list',
+    MINTING_INFORMATION_URL: 'http://localhost:4001/minting-tokens',
+    MINTING_TOKEN_URL: 'http://localhost:4001/minting-tokens/minting'
+}
