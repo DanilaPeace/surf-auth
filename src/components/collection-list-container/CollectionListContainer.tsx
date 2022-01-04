41fdef215dfeaf8407adc768d072143848d8494a
Danila Pisarev <jobdan@bk.ru>
2022-01-04 16:46:06 +0300

    Added the preloader for collection list page

--- src/components/collection-list-container/CollectionListContainer.tsx
+++ src/components/collection-list-container/CollectionListContainer.tsx
@@ -2,26 +2,31 @@ import { useEffect, useState } from "react";
 import CollectionListItem from "../collection-list-item/CollectionListItem";
 
 import { global_urls } from "../../config/urls";
+import PagePreloader from "../common/page-preloader/PagePreloader";
 
 const CollectionListContainer = () => {
   const [collectionList, setCollectionList] = useState([]);
+  const [dataIsLoaded, setDataIsLoaded] = useState(false);
 
   const getCollectionListFromServer = () => {
     fetch(global_urls.COLLECTION_LIST_URL)
       .then((res) => res.json())
       .then((data) => {
         setCollectionList(data.collectionList);
+        setDataIsLoaded(true);
       })
       .catch((err) => console.error(err));
   };
 
   useEffect(getCollectionListFromServer, []);
 
+  const collections = collectionList.map((collection, idx) => (
+    <CollectionListItem key={idx} {...collection} />
+  ));
+
   return (
     <div className="CollectionListContainer container">
-      {collectionList.map((collection, idx) => (
-        <CollectionListItem key={idx} {...collection} />
-      ))}
+      {dataIsLoaded ? collections : <PagePreloader />}
     </div>
   );
 };
