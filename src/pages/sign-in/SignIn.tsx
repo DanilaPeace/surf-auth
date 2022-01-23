8e39bc01535c8808498a3098caaa338fdabd4f68
Danila Pisarev <jobdan@bk.ru>
2022-01-23 09:43:41 +0300

    Added the package for getting data from EVER Wallet

--- src/pages/sign-in/SignIn.tsx
+++ src/pages/sign-in/SignIn.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import ton from "ton-inpage-provider";
 
 const SignIn = () => {
-  const onSignClick = (event) => {
-    alert("Hello!");
+  const onSignClick = async (event) => {
+    event.preventDefault();
+
+    await ton.ensureInitialized();
+
+    const { accountInteraction } = await ton.requestPermissions({
+      permissions: ["tonClient", "accountInteraction"],
+    });
+    if (accountInteraction == null) {
+      throw new Error("Insufficient permissions");
+    }
+
+    console.log("ACCOUNT: ", accountInteraction);
   };
 
   return (
