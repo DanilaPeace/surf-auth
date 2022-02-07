b9ecb44fa3b99fd9b4b6488af88494ddc45fdd3d
Danila Pisarev <jobdan@bk.ru>
2022-02-07 20:11:17 +0300

    Added react-qrcode to present qrcode

--- src/pages/sign-in/SignIn.tsx
+++ src/pages/sign-in/SignIn.tsx
@@ -2,6 +2,8 @@ import { observer } from "mobx-react";
 import React, { FC, useContext } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ProviderRpcClient } from "ton-inpage-provider";
+import QRCode from "react-qr-code";
+
 import { Context } from "../..";
 import PagePreloader from "../../components/common/page-preloader/PagePreloader";
 
@@ -60,15 +62,17 @@ const SignIn: FC = (props) => {
     return <PagePreloader />;
   }
 
-  const signSerf = () => {
-    
-  }
+  // const signSerf = () => {
+
+  //   return qrCanvas;
+  // }
 
   return (
     <div className="signin">
       <div className="signin__container container">
         <div className="signin__content">
           <div className="signin__btns">
+            <QRCode value="https://uri.ton.surf/debot/0:a7e1c39a6e0d59622f5e5e5d1836eccea86288c3972c15fabad56dafc57e746b?net=devnet&message=te6ccgEBBAEArAADaGIAU_DhzTcGrLEXry8ujBt2Z1QxRGHLlgr9XWq21-K_OjWAAAAAAAAAAAAAAAAAAH9IYB8DAgEASmh0dHA6Ly9sb2NhbGhvc3Q6NDAwMS9hdXRoL2RlYm90LWF1dGgASDg0NzJjMzRlLTA0ZGItNDZmYi05YmQ4LTlkZDk5OGNkOTRjOQBIYWE5NjNmODEtZGUxMy00MDIzLTk0ODItNTNhNGI3YjE4NzBh" />
             <button className="btn btn-blue" onClick={onSignClick}>
               Sign with EVER Wallet
             </button>
